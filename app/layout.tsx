368ef244c9d97b48017ac284b008c27663afda55
yasirghouri <yasir.ag7@gmail.com>
2023-09-01 12:41:39 +0500

    added dark-light theme toggle functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
-// import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ThemeProvider } from "@/components/providers/theme-provider";
 // import { ModalProvider } from '@/components/providers/modal-provider'
 // import { SocketProvider } from '@/components/providers/socket-provider'
 // import { QueryProvider } from '@/components/providers/query-provider'
@@ -25,20 +25,19 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
-          {children}
-          {/* <ThemeProvider
+          <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <SocketProvider>
+            {children}
+            {/* <SocketProvider>
               <ModalProvider />
               <QueryProvider>
-                {children}
               </QueryProvider>
-            </SocketProvider>
-          </ThemeProvider> */}
+            </SocketProvider> */}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
